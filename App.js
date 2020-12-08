0db3cbc70ea83ad8b882ec76ff319b3a624272d9
awandev <awal.kurniawan@uin-alauddin.ac.id>
2020-12-08 22:23:14 +0800

    add splash screen use react-native-onboarding-swiper

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OnboardingScreen from './screens/OnboardingScreen'
+import LoginScreen from './screens/LoginScreen'
+
+import AsyncStorage from '@react-native-community/async-storage'
+
+const AppStack = createStackNavigator();
+const App = () => {
+
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch()
+      }
+    });
+  }, [])
+
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch === true) {
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+          <AppStack.Screen name="Login" component={LoginScreen} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    )
+  } else {
+    <LoginScreen />
+  }
+
+}
+
+
+
+export default App;
